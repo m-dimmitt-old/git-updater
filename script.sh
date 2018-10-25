d41fb1c8ea288da64fc8f17a35e81ae616a8aeec
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-10-24 23:54:13 -0400

    git pull master, git pull dev, to a temp branch then confirm move to curr branch

--- script.sh
+++ script.sh
@@ -1,3 +1,51 @@
-p(){
-  git pull origin "$(git branch | grep '*' | cut -c 3-)":"$*";
-}
+hp(){
+  echo "commands are:
+  gpm
+  gpd
+  gf
+  grevert
+  gcustom"
+};
+
+##
+## In the future this will allow remotes to be passed.
+## Additionally forks will automatically get their parent project as a remote.
+##
+
+gpm(){ # git pull master:ToCurrentBranch via Temp
+  git branch -D temp-copy;
+  git checkout -b temp-copy
+  git pull origin master:"$(git branch | grep '*' | cut -c 3-)";
+  echo "merging all the changes into a temp branch. resolve conflicts,
+  upon conclusion run gf to update the branch you were on and conclude the changes!"
+
+  echo "Use grevert to abort this attempt and get placed back on the branch that you were on!"
+};
+
+gpd(){ # git pull dev:ToCurrentBranch via Temp
+  git branch -D temp-copy;
+  git checkout -b temp-copy;
+  git pull origin dev:"$(git branch | grep '*' | cut -c 3-)";
+  echo "merging all the changes into a temp branch. resolve conflicts,
+  upon conclusion run gf to update the branch you were on and conclude the changes!";
+
+  echo "Use grevert to abort this attempt and get placed back on the branch that you were on!";
+};
+
+gcustom(){ # git pull custom:ToCurrentBranch via Temp
+  git branch -D temp-copy;
+  git checkout -b temp-copy;
+  git pull origin "$*":"$(git branch | grep '*' | cut -c 3-)";
+};
+
+gf(){
+  git checkout -;
+  git merge temp-copy;
+  git branch -D temp-copy;
+};
+
+grevert(){
+  git checkout temp-copy;
+  git checkout -;
+  git branch -D temp-copy;
+};
